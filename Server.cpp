e0246853e530b2b32243adc8fa1b6ea20de84619
Amr Saeed <amrsaeed@aucegypt.edu>
2017-10-28 12:19:57 +0200

    Created Server.cpp

---
+++ Server.cpp
@@ -0,0 +1,25 @@
+#include "Server.h"
+
+Server::Server(char * _listen_hostname, int _listen_port){
+
+}
+
+Message* Server::getRequest(){
+
+}
+
+Message* Server::doOperation(){
+
+}
+
+void Server::sendReply(Message* _message){
+
+}
+
+void Server::serveRequest(){
+
+}
+
+Server::~Server(){
+
+}

8477f358a8df05ed37fcd00b52167e32ffb29e1e
Heba Sakr <hebasakr@aucegypt.edu>
2017-11-18 21:24:34 +0200

    Created MessagDecoder.cpp

---
+++ MessageDecoder.cpp
@@ -0,0 +1,17 @@
+#include "MessageDecoder.h"
+
+MessageDecoder::MessageDecoder(){
+
+}
+
+MessageDecoder::~MessageDecoder(){
+
+}
+
+Message* MessageDecoder::encode(std::vector<std::string>){
+
+}
+
+std::vector<std::string> MessageDecoder::decode(Message* _message){
+    
+}

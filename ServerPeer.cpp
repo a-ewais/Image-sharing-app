a3cc44a1b96b7403ac3963a725f014f05d9a1826
Amr Saeed <amrsaeed@aucegypt.edu>
2017-11-21 12:03:32 +0200

    Implemented Image functions of Peer Server

--- ServerPeer.cpp
+++ ServerPeer.cpp
@@ -1,10 +1,15 @@
 #include "ServerPeer.h"
 #include "Parameter.h"
 #include "MessageDecoder.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 ServerPeer::ServerPeer(char* _listen_hostname, int _listen_port):Server(_listen_hostname, _listen_port){
+	system("mkdir MyImages");
+	system("mkdir LoadedImages");
 }
 
 ServerPeer::~ServerPeer(){
@@ -17,7 +22,22 @@ std::vector<std::string> ServerPeer::getListofImages(std::string token, std::str
 }
 
 std::string ServerPeer::getImage(std::string token, std::string userID, std::string imageID){
-	//TODO: LOAD IMAGES
+	//TODO: AUTHENTICATE WITH SERVER DISCOVERY
+	streampos size;
+	char * memblock;
+
+	ifstream file (imageID, ios::in|ios::binary|ios::ate);
+	if (file.is_open()){
+		size = file.tellg();
+		memblock = new char [size];
+		file.seekg (0, ios::beg);
+		file.read (memblock, size);
+		file.close();
+		string image(memblock, size);
+		delete[] memblock;
+		return image;
+	}
+	else return NULL;
 }
 
 void ServerPeer::updateViews(std::string token, std::string userID, int count){
@@ -40,11 +60,23 @@ Message* ServerPeer::doOperation(Message* _message){
     MessageDecoder::decode(_message, args);
 
     switch(operation){
-    case 7:
+    case 7:{
+    	std::vector<std::string> imageList = getListofImages(args[0].getString(), args[1].getString());
+    	Parameter arg1;
+		arg1.setVectorString(imageList);
+		reply_args.push_back(arg1);
+    }
     	break;
-    case 8:
+    case 8:{
+    	string image = getImage(args[0].getString(), args[1].getString(), args[2].getString());
+    	Parameter arg1;
+    	arg1.setString(image);
+    	reply_args.push_back(arg1);
+    }
     	break;
-    case 9:
+    case 9:{
+
+    }
     	break;
     }
 

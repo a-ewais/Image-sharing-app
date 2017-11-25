492e304a6add0ed6b6d68e83677ce128879d71b3
Amr Saeed <amrsaeed@aucegypt.edu>
2017-11-25 14:44:54 +0200

    Added database to ServiceDiscovery

--- ServiceDiscovery.cpp
+++ ServiceDiscovery.cpp
@@ -15,7 +15,7 @@ ServiceDiscovery::ServiceDiscovery(char* _listen_hostname, int _listen_port):Ser
 
 bool ServiceDiscovery::auth(string username, string token){
 	cout << "SERVICEDISCOVERY::AUTH!";
-    if(token_user[token]==username)
+    if(DL.retreive(username, "TOKEN")==token)
         return true;
     return false;
 } //check if in token_user? true: false;
@@ -24,13 +24,15 @@ string ServiceDiscovery::signUp(string username, string password, string addr){
 	cout << "SERVICEDISCOVERY::SIGNUP!";
     srand(time(NULL));
     string token;
-    users[username] = password;
+//    users[username] = password;
     do{
         token = to_string(rand()%10000);
     }while(token_user.count(token)>0);
 
-    token_user[token] = username;
-    online_users[username]=addr;
+    DL.insert(username, password, addr, true, token);
+
+//    token_user[token] = username;
+//    online_users[username]=addr;
     // pending_requests needs username or token?
     return token;
 }
@@ -39,26 +41,37 @@ string ServiceDiscovery::signIn(string username, string password, string addr){
 	cout << "SERVICEDISCOVERY::SIGNIN!";
     srand(time(NULL));
     string token;
-    if(users[username]==password){
+    if(DL.retreive(username, "PASSWORD")==password){
         do{
             token = to_string(rand()%10000);
         }while(token_user.count(token)>0);
 
-        token_user[token] = username;
-        online_users[username]=addr;
+        DL.update(username, "TOKEN", token);
+		DL.update(username, "ONLINE", "true");
+		DL.update(username, "IP", addr);
+//        token_user[token] = username;
+//        online_users[username]=addr;
         // pending_requests needs username or token?
         return token;
     }
-    token = "";
-    return token;
+    return token = "";
 }  //check if in (users)?generate random token..add to token_user..check if he has pending requests..receive ack...return token: return error.
 
 map<string, string> ServiceDiscovery::getListOfOnlineUsers(string username, string token){
 	cout << "SERVICEDISCOVERY::getListofOnlineUsers!";
+    vector<string> rec1, rec2;
+
     if(auth(username,token)){
     	return online_users;
     }
-    std::map<string, string> mss;
+
+    map<string, string> mss;
+    rec1 = DL.getOnline("USERNAME");
+    rec2 = DL.getOnline("IP");
+
+    for (int i = 0; i < rec1.size(); i++)
+    	mss[rec1[i]] = rec2[i];
+
     return mss;
 }
 
@@ -67,14 +80,16 @@ void ServiceDiscovery::stillUp(string username, string token, string addr){
     if(auth(username,token)){
         token_time[token]++;
     }
-    online_users[username]=addr;
+    DL.update(username, "IP", addr);
+//    online_users[username]=addr;
 } //auth()? update time_cnt in token_time: ignore
 
 void ServiceDiscovery::down(string username, string token){
 	cout << "SERVICEDISCOVERY::DOWN!";
     if(auth(username,token)){
         token_time.erase(token);
-        online_users.erase(username); 
+//        online_users.erase(username);
+        DL.update(username, "ONLINE", "0");
     }
 }
 
